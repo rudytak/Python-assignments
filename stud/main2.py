f5cc829e7a4ce57bbc0b324ffb7b35c3a70f4ed0
rudytak <75335232+rudytak@users.noreply.github.com>
2022-05-06 08:56:51 +0200

    bara

--- stud/main2.py
+++ stud/main2.py
@@ -9,7 +9,8 @@ def nacteni_do_slovniku():
                 "surname": jmeno[1],
                 "id": lines[i+1].strip(),
                 "file": lines[i+2].strip(),
-                "percent": []
+                "percent": [],
+                "average": 0
             })
     return list
 
@@ -18,10 +19,24 @@ def nacteni_procent(list):
     for i in list:
         with open(list[x]["file"]) as f:
             for number in f.readlines():
-                list[x]["percent"].append(number)
-            x = x+1
-    print(list)
+                list[x]["percent"].append(int(number.strip()))
+            avg_value = sum (list[x]["percent"]) / len (list[x]["percent"])
+            list[x]["average"] = avg_value
+            x = x + 1
+            #print(list)
     return list
 
-data = nacteni_do_slovniku()
-nacteni_procent(data)
+def kompet_data(list):
+    x = 0
+    text = ""
+    for i in list:
+        text+=list[x]["name"] + ";" + list[x]["surname"] + ";" + list[x]["id"] + ";" + ";".join(str(p) for p in list[x]["percent"][0:10])+";"+str(list[x]["average"])+"00\n"
+        x = x + 1
+    
+    f = open("output.csv", "a")
+    f.write(text)
+    return
+
+list = nacteni_do_slovniku()
+list = nacteni_procent(list)
+(kompet_data(list))
