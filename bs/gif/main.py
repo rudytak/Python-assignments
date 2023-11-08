df9bb863607c97301a3fefddf74172c6650698b1
rudytak <ondra4sedlacek@gmail.com>
2023-11-08 23:20:30 +0100

    f

--- bs/gif/main.py
+++ bs/gif/main.py
@@ -3,7 +3,9 @@ import random, math
 from tkinter import *  # Python 3
 import imageio
 
-leng = 30
+leng = 100
+r_div = 100
+seed = 10
 
 def turtle_headto(t, x, y):
 	t.left(t.towards(x, y) - t.heading() - 90)
@@ -27,7 +29,7 @@ t.speed(0)
 t.hideturtle()
 
 for time in range(0, leng):
-	random.seed(10)
+	random.seed(seed)
 
 	bg(t, "black")
 	t.pen(pencolor="white", pensize="1", fillcolor="white")
@@ -41,7 +43,7 @@ for time in range(0, leng):
 
 	for i in range(7):
 
-		a = random.random() * 3.141 + i*time/25
+		a = random.random() * 3.141 + i*time/r_div
 		t.up()
 		t.goto(math.cos(a) * 30 * i, math.sin(a) * 30 * i)
 		turtle_headto(t, 0, 0)
@@ -66,7 +68,7 @@ for time in range(0, leng):
 		t.down()
 
 		for j in range(i):
-			aa = random.random() * 3.141 - j*time/25
+			aa = random.random() * 3.141 - j*time/r_div
 			t.up()
 			t.goto(
 				math.cos(a) * 30 * i + math.cos(aa) * 10 * j,
@@ -81,8 +83,15 @@ for time in range(0, leng):
 	ts = turtle.getscreen()
 	ts.getcanvas().postscript(file="./"+str(time)+".eps")
 
-# images = []
-# for i in range(0,leng):
-# 	images.append(imageio.imread("./"+str(i)+".eps"))
+# source: https://stackoverflow.com/questions/44587376/oserror-unable-to-locate-ghostscript-on-paths/63262348#63262348
+from PIL import EpsImagePlugin, Image
+EpsImagePlugin.gs_windows_binary = r'C:/Program Files/gs/gs10.00.0/bin/gswin64c'
 
-# imageio.mimsave('/gif.gif', images)
+images = []
+for i in range(0,leng):
+	im = Image.open("./"+str(i)+".eps")
+	im.save("./"+str(i)+".png")
+
+	images.append(imageio.imread("./"+str(i)+".png"))
+
+imageio.mimsave('./gif.gif', images)
